06540030cc77bd38a352632066a3ab1f81120e85
B1Z1 <iluhamishkin@gmail.com>
2022-06-14 17:38:28 +0200

    initial commit

---
+++ apps/nx-error/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-error-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nx-error';
+}
